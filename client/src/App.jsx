236d8b896cb953846197611e7258da659bd84008
DaniIliev <114102749+DaniIliev@users.noreply.github.com>
2023-12-01 20:35:51 +0200

    add Authprovider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import Catalog from './components/catalog/Catalog'
 import GameDetails from './components/details/GameDetails'
 import * as userService from './services/userService'
 import AuthContext from './context/userContext'
-import Logout from './components/logout/logout'
+import Logout from './components/logout/Logout'
 
 function App() {
   const [auth, setAuth] = useState(() => {
@@ -20,63 +20,28 @@ function App() {
 
   const navigate = useNavigate()
 
-  const loginSubmitHandler = async (values) => {
-    try{
-      const result = await userService.login(values.email, values.password)
-      setAuth(result)
 
-      localStorage.setItem('accessToken', result.accessToken);
 
-      navigate('/')
-    }catch(err){
-      console.log(err)
-    }
-  }
 
-  const registerSubmitHandler = async (values) =>{
-    try{
-      const result = await userService.register(values.email, values.password, values.confirmPassword)
-      setAuth(result)
 
-      localStorage.setItem('accessToken', result.accessToken);
-
-      navigate('/')
-    }catch(err){
-      console.log(err)
-    }
-  }
-
-  const logoutHandler = async (values) => {
-      setAuth({})
-
-      localStorage.removeItem('accessToken')
-  }
-  const values = {
-    loginSubmitHandler,
-    registerSubmitHandler,
-    logoutHandler,
-    username: auth.username || auth.email,
-    email: auth.email,
-    isAuthenticated: !!auth.accessToken,
-  }
   return (
     <>
-    <AuthContext.Provider value={values}>
-    <div id='box'>
-      <Header/>
-      
-      <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/all-games' element={<Catalog/>}/>
-          <Route path='/game-details/:gameId' element={<GameDetails/>}/>
-          <Route path='/user/login' element={<Login/>} />
-          <Route path='/user/register' element={<Register/>}/>
-          <Route path='/user/logout' element={<Logout/>}/>
-          <Route path='/create-game' element={<Create/>} />
-      </Routes>
+    <AuthProvider>
+        <div id='box'>
+          <Header/>
+          
+          <Routes>
+              <Route path='/' element={<Home/>}/>
+              <Route path='/all-games' element={<Catalog/>}/>
+              <Route path='/game-details/:gameId' element={<GameDetails/>}/>
+              <Route path='/user/login' element={<Login/>} />
+              <Route path='/user/register' element={<Register/>}/>
+              <Route path='/user/logout' element={<Logout/>}/>
+              <Route path='/create-game' element={<Create/>} />
+          </Routes>
 
-    </div>
-    </AuthContext.Provider>
+        </div>
+    </AuthProvider>
     </>
   )
 }
--- client/src/context/userContext.js
+++ client/src/context/userContext.js
@@ -1,5 +1,59 @@
 import { createContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AuthContext = createContext()
 
 export default AuthContext
+
+export const AuthProvider = ({
+    children
+}) => {
+    const navigate = useNavigate()
+
+    const loginSubmitHandler = async (values) => {
+        try{
+          const result = await userService.login(values.email, values.password)
+          setAuth(result)
+    
+          localStorage.setItem('accessToken', result.accessToken);
+    
+          navigate('/')
+        }catch(err){
+          console.log(err)
+        }
+      }
+
+
+      const registerSubmitHandler = async (values) =>{
+        try{
+          const result = await userService.register(values.email, values.password, values.confirmPassword)
+          setAuth(result)
+    
+          localStorage.setItem('accessToken', result.accessToken);
+    
+          navigate('/')
+        }catch(err){
+          console.log(err)
+        }
+      }
+    
+      const logoutHandler = async (values) => {
+        setAuth({})
+  
+        localStorage.removeItem('accessToken')
+    }
+        const values = {
+            loginSubmitHandler,
+            registerSubmitHandler,
+            logoutHandler,
+            username: auth.username || auth.email,
+            email: auth.email,
+            isAuthenticated: !!auth.accessToken,
+        }
+
+        return(
+            <AuthContext.Provider value={values}>
+                {children}
+            </AuthContext.Provider>
+        )
+}
